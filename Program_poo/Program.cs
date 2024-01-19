7edb9abecb51429acf46da734304059ae31ace8d
Romain-GRADELET <romain.gradelet73@outlook.fr>
2024-01-19 11:32:58 +0100

    Définition de d'une interface Affichable

--- Program_poo/Program.cs
+++ Program_poo/Program.cs
@@ -6,20 +6,31 @@ namespace program_poo
     class Enfant : Etudiant
     {
         string classeEcole;
+        Dictionary<string, float> notes;
 
-        public Enfant(string nom_param, int age_param, string classeEcole) : base(nom_param, age_param, null)
+        public Enfant(string nom_param, int age_param, string classeEcole, Dictionary<string, float> notes) : base(nom_param, age_param, null)
         {
             this.classeEcole = classeEcole;
+            this.notes = notes;
         }
         public override void Afficher()
         {
             AfficherNomEtAge();
             Console.WriteLine(" Enfant en classe de " + classeEcole);
+
+            if ((notes != null)&&(notes.Count != 0)){
+                Console.WriteLine("Notes moyennes : ");
+                foreach (var note in notes)
+                {
+                    Console.WriteLine("  " + note.Key + " : " + note.Value + " / 10");
+                }
+            }
+            
             AfficherProfesseurPrincipal();
         }
     }
 
-    class Etudiant : Personne
+    class Etudiant : Personne 
     {
         protected string infoEtude;
         public Personne professeurPrincipal { get; init; }
@@ -39,7 +50,7 @@ namespace program_poo
         {
             if (professeurPrincipal != null)
             {
-                Console.WriteLine(" Le professeur principal est : ");
+                Console.WriteLine("Le professeur principal est : ");
                 professeurPrincipal.Afficher();
             }
             else
@@ -47,16 +58,17 @@ namespace program_poo
                 Console.WriteLine(" Aucun professeur principal spécifié");
             }
         }
-
     }
 
-    class Personne
+    class Personne : IAffichable
     {
         static int nombreDePersonne = 0;
 
-        protected string nom;
-        protected int age;
+        public string nom {  get; init; }
+        public int age { get; init; }
+
         protected string emploi;
+
         protected int numeroPersonne;
 
         public Personne(string nom_param, int age_param, string emploi_param = null)
@@ -97,55 +109,85 @@ namespace program_poo
         }
     }
 
+    class TableMultiplication : IAffichable
+    {
+        int numero;
+
+        public TableMultiplication(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Table de multiplication de " + numero);
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine(i + " x " + numero + " = " + (i * numero));
+            }
+        }
+    }
+
+    interface IAffichable
+    {
+        void Afficher()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            /*var noms = new List<string>() {"Paul", "Jacques", "David", "Juliette" };
-            var ages = new List<int>() { 30, 35, 20, 8 };
-            var emplois = new List<string>() { "Développeur", "Professeur", "Etudiant", "CP" };
-
-            for (int i = 0; i < noms.Count; i++)
-            {
-                AfficherInfoPersonne(noms[i], ages[i], emplois[i]);
-            }*/
-
-            //Personne personne1 = new Personne("Paul", 30, "Développeur");
-            //personne1.Afficher();
-
-            //Personne personne2 = new Personne("Jacques", 35, "Professeur");
-            //personne2.Afficher();
-
-            //---------------------------------------------------------------
-
-            /*var personnes = new List<Personne>()
+            var elements = new List<IAffichable>()
             {
                 new Personne("Paul", 30, "Développeur"),
                 new Personne("Jacques", 35, "Professeur"),
-                new Personne("David", 20, "Etudiant"),
-                new Personne("Juliette", 8, "CP")
+                new Etudiant("David", 20, "Etudiant"),
+                new Enfant("Juliette", 8, "CP", null),
+                new TableMultiplication(2)
             };
 
+            //********************
+            //**** Where & is ****
+            //********************
             //personnes = personnes.OrderBy(p => p.nom).ToList();
-
-            foreach (var personne in personnes)
+            //personnes = personnes.Where(p => p.age >= 25).ToList();
+            //personnes = personnes.Where(p => p is Etudiant).ToList();
+            //personnes = personnes.Where(p => (p.nom[0] == 'J')&&(p.age >= 25)).ToList();
+            foreach (var element in elements)
             {
-                personne.Afficher();
-                Personne.AfficherNombreDePersonne();
-            }*/
+                element.Afficher();
+                //Personne.AfficherNombreDePersonne();
+            }
+
+            Console.WriteLine();
+
+            //var table = new TableMultiplication(2);
+            //table.Afficher();
+
+            //------------------------------------------------------------------------
 
             //var personne1 = new Personne { nom = "Paul", age = 20, emploi = "ingénieur" };
             //var personne2 = new Personne { nom = "Jacques", age = 35};
 
             //var personne1 = new Personne("Paul", 35, "Professeur");
 
-            var etudiant = new Etudiant("David", 20, "école d'ingénieur informatique") { professeurPrincipal = new Personne("Paul", 35, "Professeur") };
+            /*var etudiant = new Etudiant("David", 20, "école d'ingénieur informatique") { professeurPrincipal = new Personne("Paul", 35, "Professeur") };
             etudiant.Afficher();
 
             Console.WriteLine();
 
-            var enfant = new Enfant("Sophie", 8, "CP") { professeurPrincipal = new Personne("Jean", 35, "Professeur des écoles") };
-            enfant.Afficher();
+            var NoteMoyenne = new Dictionary<string, float> {
+                { "Maths", 5f },{ "Geo", 8.5f },{"Dictée", 3.3f}
+            };
+
+            var enfant = new Enfant("Sophie", 8, "CP", null)
+            {
+                professeurPrincipal = new Personne("Jean", 35, "Professeur des écoles")
+            };
+            enfant.Afficher();*/
 
         }
     }
